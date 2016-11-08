8a78e78e454960b5490a72f3f96b55e736fe4a56
Richard Ziegler <nablith@gmail.com>
2016-11-08 05:25:46 -0600

    Added spaces because seriously Xcode

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -8,27 +8,40 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
     
-    @IBOutlet weak var theLabel: UILabel!
-    var tapCounter = 0
     
-    @IBAction func tapCount(_ sender: Any) {
-        tapCounter += 1
-        print(tapCounter)
-        if tapCounter >= 10 {
-            print("Stop that!")
-        }
-    }
     
-    @IBAction func buttonPress(_ sender: Any) {
-    }
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
 
     override func didReceiveMemoryWarning() {
